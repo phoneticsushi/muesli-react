9e7e1c7f4b015419e4ff8a2018b4949455298068
Ryan Weisman <5532320+phoneticsushi@users.noreply.github.com>
2021-12-20 20:17:52 -0600

    Remove debugging readouts from AudioDisplay

--- src/AudioDisplay.js
+++ src/AudioDisplay.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState, } from "react";
+import { useEffect, useRef } from "react";
 import { Box, Button } from '@mui/material';
 import WaveSurfer from "wavesurfer.js";
 import { useTheme } from '@mui/material/styles';
@@ -9,16 +9,8 @@ function AudioDisplay(props) {
   const waveSurferDomRef = useRef();
   const waveSurferRef = useRef();
 
-  const [loadingProgress, setLoadingProgress] = useState();
-
-  // Debugging
-  const [waveSurferLoadCount, setWaveSurferLoadCount] = useState(0);
-  const [updatedPropsCount, setUpdatedPropsCount] = useState(0);
-
   useEffect(
     () => {
-      setWaveSurferLoadCount(waveSurferLoadCount + 1)  //Debugging 
-
       // WaveSurfer requires the DOM in which it's rendered
       // to be loaded on the page before calling its create function.
       // To compensate, we initialize the WaveSurfer object once as a side-effect
@@ -29,8 +21,7 @@ function AudioDisplay(props) {
             responsive: true,
           }
         )
-      waveSurferRef.current.on('loading', progress => { setLoadingProgress(progress) })
-
+      
       // returned function will be called on component unmount
       return () => {
         waveSurferRef.current.destroy()
@@ -69,7 +60,6 @@ function AudioDisplay(props) {
       } else {
         waveSurferRef.current?.empty()
       }
-      setUpdatedPropsCount(updatedPropsCount + 1)  //Debugging
     },
     [props.audioPath],
   );
@@ -97,10 +87,7 @@ function AudioDisplay(props) {
   return (
     <Box>
       <h2>AudioDisplay WIP</h2>
-      <p>WaveSurfer load count: {waveSurferLoadCount}</p>
-      <p>Updated props count: {updatedPropsCount}</p>
       <p>Audio Path: {props.audioPath}</p>
-      <p>Loading Progress: {loadingProgress}</p>
       <div ref={waveSurferDomRef}></div>
       <Button onClick={play}>Play Audio</Button>
       <Button onClick={pause}>Pause Audio</Button>
