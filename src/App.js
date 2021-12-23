c48bbf67eae1d430f05b2909dffff6aaf905475d
Ryan Weisman <5532320+phoneticsushi@users.noreply.github.com>
2021-12-22 23:02:56 -0600

    Remove unused import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { CssBaseline } from '@mui/material';
 import './App.css';
 
-import LandingPage from './LandingPage.js';
+// import LandingPage from './LandingPage.js';
 import Dictaphone from './Dictaphone.js';
 
 function App() {
