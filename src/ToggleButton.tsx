7f79c2c2fad897bb5ff0e0500b374bb1b97228ed
Ryan Weisman <5532320+phoneticsushi@users.noreply.github.com>
2021-12-24 15:24:23 -0600

    Filter props in ToggleButton

--- src/ToggleButton.tsx
+++ src/ToggleButton.tsx
@@ -1,16 +1,18 @@
 import { Button } from '@mui/material';
 
 type ToggleButtonProps = {
-  enabled: boolean,
-  enableText: string,
-  disableText: string
+  enabled?: boolean,
+  enableText?: string,
+  disableText?: string
 }
 
 function ToggleButton(props: ToggleButtonProps) {
+  const {enabled, enableText, disableText, ...passthroughProps} = props
+
   return (
     <Button
       color={props.enabled ? "error" : "success"}
-      {...props}
+      {...passthroughProps}
     >
       {props.enabled ? (props.disableText || "Disable") : (props.enableText || "Enable")}
     </Button>
